b61122b2fa7bf9cfb158d3e97edbe263edec4e0c
PrinceYraldo16 <silencieuxyraldo.com>
2025-11-17 18:36:11 -0500

    Premier cmmit projets

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.flashcardapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
@@ -17,9 +19,33 @@ class MainActivity : AppCompatActivity() {
         val FlashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
         val FlashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcard_answe1 = findViewById<TextView>( R.id.flashcard_answer1)
+        val FlashcardAnswer2 = findViewById<TextView>(R.id.flashcard_answer2)
+        val BtnSui = findViewById<Button>(R.id.BtnS)
 
         FlashcardAnswer.setOnClickListener {
+            findViewById<View>(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.redcolor, null))
+            findViewById<View>(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.white, null))
+            findViewById<View>(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.white, null))
+        }
 
+        FlashcardAnswer2.setOnClickListener {
+            findViewById<View>(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.Vert, null))
+            findViewById<View>(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.white, null))
+            findViewById<View>(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.white, null))
+        }
+
+        flashcard_answe1.setOnClickListener {
+            findViewById<View>(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.redcolor, null))
+            findViewById<View>(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.white, null))
+            findViewById<View>(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.white, null))
+        }
+
+        BtnSui.setOnClickListener {
+
+        }
+
+        BtnSui.setOnClickListener {
+            setContentView(R.layout.activite1)
         }
 
         FlashcardQuestion.setOnClickListener {
